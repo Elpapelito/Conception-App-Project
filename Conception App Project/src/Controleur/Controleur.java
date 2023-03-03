78eeaf251d35d225fbd8b70b6ee914bdb1dc55ba
Elpapelito <d.redguns45@gmail.com>
2023-03-03 23:09:29 +0100

    Commit 3

--- Conception App Project/src/Controleur/Controleur.java
+++ Conception App Project/src/Controleur/Controleur.java
@@ -1,4 +1,57 @@
 package Controleur;
+import Vue.Ihm;
+import Modele.*;
 
 public class Controleur {
+
+    Ihm ihm;
+    Damier damier;
+    Joueur[] lesJoueurs;
+
+
+    public Controleur( Ihm ihm )
+    {
+        this.ihm = ihm;
+
+    }
+
+
+    public void jouer(){
+        String message = "_______________________________________\n" +
+                         "|      Bienvenue dans nos projet      |\n" +
+                         "|     de Conception d'application!    |\n" +
+                         "|                                     |\n" +
+                         "|        Choisissez votre jeu:        |\n" +
+                         "|                                     |\n" +
+                         "|    @ Othello Joueur Vs Joueur (1)   |\n" +
+                         "|_____________________________________|\n" +
+                         "Reponse : ";
+
+        String jeu = "0";
+        while (!jeu.equals("1")){
+            ihm.affiche("coucou");
+            jeu = ihm.saisieString(message);
+            message = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
+                    "_______________________________________\n" +
+                    "|    Le nombre saisie ne correspond   |\n" +
+                    "|             à aucun jeu.            |\n" +
+                    "|                                     |\n" +
+                    "|        Choisissez votre jeu:        |\n" +
+                    "|                                     |\n" +
+                    "|    @ Othello Joueur Vs Joueur (1)   |\n" +
+                    "|_____________________________________|\n" +
+                    "Reponse : ";
+
+        }
+
+        damier = new Damier(8);
+        ihm.affiche(damier);
+
+
+
+    }
+
+
+
+
 }
--- Conception App Project/src/Modele/Joueur.java
+++ Conception App Project/src/Modele/Joueur.java
@@ -1,4 +1,42 @@
 package Modele;
 
 public class Joueur {
+
+    String nom ;
+    String color;
+    int partieGagne;
+
+
+    public Joueur( String name , char color)
+    {
+        this.nom = name;
+        if (color == 'B'){
+            this.color = "\\u26AA";
+        }
+        else
+        {
+            this.color = "\\u26AB";
+        }
+        this.partieGagne = 0;
+
+
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public String getPion(){
+        return this.color;
+
+    }
+
+    public int getPartieGagne(){
+        return this.partieGagne;
+    }
+
+    public void setPartieGagne(int newInt){
+        this.partieGagne = newInt;
+    }
+
 }
